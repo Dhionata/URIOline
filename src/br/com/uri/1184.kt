4f0f7e7af372948c252ed1da7946945971fbe194
Dhionata <xiyunagi@gmail.com>
2020-01-22 19:13:41 -0300

    Adicionado exercício 1184

---
+++ src/br/com/uri/1184.kt
@@ -0,0 +1,20 @@
+package br.com.uri
+
+fun main(args: Array<String>) {
+    val matriz = Array(12) { DoubleArray(12) }
+    val operacao = readLine()
+    var soma = 0.0
+    for (linha in matriz.indices) {
+        for (coluna in matriz.indices) {
+            matriz[linha][coluna] = readLine()!!.toDouble()
+            if (coluna < linha) {
+                soma += matriz[linha][coluna]
+            }
+        }
+    }
+
+    when (operacao) {
+        "S" -> println("%.1f".format(soma))
+        "M" -> println("%.1f".format(soma / 66))
+    }
+}
